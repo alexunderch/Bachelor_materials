c1fa94e6099a5401fad2a67919ba517e328edea8
alexunderch <73017461+alexunderch@users.noreply.github.com>
2021-03-31 12:08:38 +0300

    Update my_network.py

--- ml-mipt_advanced/Fun_NLP/my_network.py
+++ ml-mipt_advanced/Fun_NLP/my_network.py
@@ -1,12 +1,61 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
+import torch.nn.functional as F
 import random
 import math
 import time
 
-# from transformers import BertTokenizer, BertModel   
+def scaled_dot_product(q, k, v, mask = None):
+    d_k = q.size()[-1]
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    attn_logits = attn_logits / math.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -1e16)
+    attention = F.softmax(attn_logits, dim = -1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, input_dim, embed_dim, num_heads):
+        """Just almost a copy of torch.nn.MultiheadAttention"""
+        super(type(self), self).__init__()
+        assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+
+        self.qkv_proj = nn.Linear(input_dim, 2 * embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+        self._reset_parameters()
+
+
+    def _reset_parameters(self):
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+
+    def forward(self, q, c, mask = None, return_attention_weights = False):
+        batch_size, seq_length, embed_dim = q.size()
+        kv = self.qkv_proj(c)
+
+        # Separate Q, K, V from linear output
+        kv = kv.reshape(c.size(0), c.size(1), self.num_heads, 2 * self.head_dim)
+        kv = kv.permute(0, 2, 1, 3) # [batch_size, seq_len, n_heads, dims]
+        k, v = kv.chunk(2, dim = -1)
+        q = q.reshape(batch_size, seq_length, self.num_heads, self.head_dim)
+        q = q.permute(0, 2, 1, 3)
+        # Determine value outputs
+        values, attention = scaled_dot_product(q, k, v, mask = mask)
+        values = values.permute(0, 2, 1, 3) # [batch_size, seq_len, n_heads, dims]
+        values = values.reshape(c.size(0), seq_length, embed_dim)
+        out = self.o_proj(values)
+
+        if return_attention_weights: return out, attention
+        return out
 
 class Encoder(nn.Module):
     def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout, bidirectional):
@@ -23,14 +72,9 @@ class Encoder(nn.Module):
             num_embeddings = input_dim,
             embedding_dim = emb_dim)
 
-        self.conv1 = nn.Conv1d(in_channels = emb_dim, out_channels = emb_dim, kernel_size = 2, padding = 1)
-        self.relu1 = nn.Tanh()
-        self.dropout1 = nn.Dropout(dropout)
-        self.bn1 = nn.BatchNorm1d(emb_dim)
-        self.amp = nn.AdaptiveMaxPool1d(2)      
+        self.self_attn = MultiheadAttention(emb_dim, emb_dim, 4)
 
-        self.self_attention = nn.MultiheadAttention(emb_dim, 2)
-        self.relu2 = nn.LeakyReLU()
+        self.relu = nn.ReLU()
         self.rnn = nn.LSTM(
             input_size = emb_dim,
             hidden_size = hid_dim,
@@ -38,22 +82,21 @@ class Encoder(nn.Module):
             dropout = dropout,
             bidirectional = bidirectional)
         
+        self.norm1 = nn.LayerNorm(emb_dim)
         self.dropout = nn.Dropout(p = dropout)
         
-    def forward(self, src):
+    def forward(self, src, mask = None):
         
         #src = [src sent len, batch size]
         
         # Compute an embedding from the src data and apply dropout to it
         embedded = self.embedding(src)# <YOUR CODE HERE>
-        embedded = self.conv1(embedded.transpose(1, 2))
-        embedded = self.relu1(embedded)
-        embedded = self.dropout1(embedded)
+        attn_out = self.self_attn(embedded.transpose(0, 1), embedded.transpose(0, 1))
+        embedded = embedded + self.dropout(attn_out.transpose(0, 1))
+        embedded = self.norm1(embedded)
+        
+        output, (hidden, cell) = self.rnn(embedded) 
 
-        attn_scores, _ = self.self_attention(embedded.transpose(1, 2),
-                                             embedded.transpose(1, 2),
-                                            embedded.transpose(1, 2)) 
-        output, (hidden, cell) = self.rnn(self.relu2(attn_scores))
         #embedded = [src sent len, batch size, emb dim]
         
         # Compute the RNN output values of the encoder RNN. 
@@ -64,7 +107,7 @@ class Encoder(nn.Module):
         #cell = [n layers * n directions, batch size, hid dim]
         
         #outputs are always from the top hidden layer
-        
+        # print(output.shape, hidden.shape, cell.shape)
         return output, hidden, cell
 
 class Decoder(nn.Module):
@@ -81,23 +124,26 @@ class Decoder(nn.Module):
             num_embeddings = output_dim,
             embedding_dim = emb_dim)
         
-        self.encoder_attention1 = nn.MultiheadAttention(emb_dim, 2)
-        self.relu1 = nn.LeakyReLU()
+        self.self_attention = MultiheadAttention(emb_dim, emb_dim, 4)
+
+        self.encoder_attention = MultiheadAttention(hid_dim,  hid_dim, 4)
+
+        self.relu1 = nn.ReLU()
         self.rnn = nn.LSTM(
             input_size = emb_dim,
             hidden_size = hid_dim,
             num_layers = n_layers,
             dropout = dropout,
-            bidirectional = bidirectional)
+            bidirectional = False)
 
-        self.encoder_attention2 = nn.MultiheadAttention(hid_dim, 2)
-        self.relu2 = nn.LeakyReLU()
+        self.relu2 = nn.ReLU()
         self.out = nn.Linear(
             in_features = hid_dim,
             out_features = output_dim)
-        
         self.dropout = nn.Dropout(p = dropout)
-
+        self.dropout1 = nn.Dropout(p = dropout)
+        self.dropout2 = nn.Dropout(p = dropout)
+        self.norm1 = nn.LayerNorm(emb_dim)
     def forward(self, _input, hidden, cell, encoder_outputs):
         
         #input = [batch size]
@@ -113,17 +159,16 @@ class Decoder(nn.Module):
         #input = [1, batch size]
         
         # Compute an embedding from the input data and apply dropout to it
-        embedded = self.dropout(self.embedding(encoded_input))# <YOUR CODE HERE>
-        attn_scores, _  = self.encoder_attention1(embedded, 
-                                        encoder_outputs, 
-                                        encoder_outputs)
+        embedded = self.dropout(self.embedding(encoded_input))
+        self_attn_out = self.self_attention(embedded.transpose(0, 1), 
+                                            embedded.transpose(0, 1))
+        embedded = embedded + self.dropout1(self_attn_out.transpose(0, 1)) 
+
+
         #embedded = [1, batch size, emb dim]
         
         # Compute the RNN output values of the encoder RNN. 
         # outputs, hidden and cell should be initialized here. Refer to nn.LSTM docs ;)
-        # <YOUR CODE HERE>
-        
-        
         #output = [sent len, batch size, hid dim * n directions]
         #hidden = [n layers * n directions, batch size, hid dim]
         #cell = [n layers * n directions, batch size, hid dim]
@@ -132,13 +177,11 @@ class Decoder(nn.Module):
         #output = [1, batch size, hid dim]
         #hidden = [n layers, batch size, hid dim]
         #cell = [n layers, batch size, hid dim]
-        
-        
-        output, (hidden, cell) = self.rnn(self.relu1(attn_scores), (hidden, cell))
-        attn_scores, _  = self.encoder_attention2(output, 
-                                                  output, 
-                                                  output)
-        prediction = self.out(self.relu2(attn_scores).squeeze(0))
+                
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell)) 
+        output =  output + self.dropout2(self.encoder_attention(output.transpose(0, 1), 
+                                                                encoder_outputs.transpose(0, 1)).transpose(0, 1))
+        prediction = self.out(output.squeeze(0))
         
         #prediction = [batch size, output dim]
         
@@ -158,10 +201,10 @@ class Seq2Seq(nn.Module):
         assert encoder.n_layers == decoder.n_layers, \
             "Encoder and decoder must have equal number of layers!"
         
-    def forward(self, src, trg, teacher_forcing_ratio = 0.67):
+    def forward(self, src, trg, teacher_forcing_ratio = 0.5):
         
         #src = [src sent len, batch size]
-        #trg = [trg sent len, batch size]
+        #trg = [trg   sent len, batch size]
         #teacher_forcing_ratio is probability to use teacher forcing
         #e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
         
@@ -175,7 +218,6 @@ class Seq2Seq(nn.Module):
         
         #last hidden state of the encoder is used as the initial hidden state of the decoder
         enc_outputs, hidden, cell = self.encoder(src)
-        
         #first input to the decoder is the <sos> tokens
         _input = trg[0, :]
         
